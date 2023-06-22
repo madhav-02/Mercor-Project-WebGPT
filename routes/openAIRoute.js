62f570ac53d0b78590bcf10685371ec3dca15b8a
Madhav007 <madhavaprashath669@gmail.com>
2023-06-22 12:51:19 +0530

    Integrate gpt model for query

--- routes/openAIRoute.js
+++ routes/openAIRoute.js
@@ -3,11 +3,37 @@ import * as dotenv from "dotenv";
 const router = express.Router();
 import { Configuration, OpenAIApi } from "openai";
 
-
 dotenv.config();
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
 
 router.get('/', (req, res) => {
-    res.send('Hello from Router!');
+    res.send('Hello from OpenAI Router!');
   });
+
+router.post('/', async (req, res) => {
+  try{
+
+    const prompt =req.body.prompt;
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `${prompt}`,
+        temperature: 0,
+        max_tokens: 3000,
+        top_p: 1,
+        frequency_penalty: 0.3,
+        presence_penalty: 0,
+    })
+    res.status(200).send({
+        aiResponse: response.data.choices[0].text
+    })
+}catch(error){
+    console.log(error);
+    res.status(500).send({error});
+}
+})
   
 export default router;
